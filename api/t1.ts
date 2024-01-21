725901f0b7d40f05e50d09db1b9fc6f623ed59b0
Deng Haozhi <haozhi_deng@126.com>
2024-01-21 15:40:53 +0800

    fanli-ui 0.0.6-SNAPSHOT Project creation! by Deng Haozhi

---
+++ api/t1.ts
@@ -0,0 +1,8 @@
+export interface mt {
+  id: string;
+}
+
+export default function getT1Mt(): mt {
+  const mt1: mt = { id: "" };
+  return mt1;
+}
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-app>
+    <v-btn color="success">text</v-btn>
+  </v-app>
+</template>
+
+<script lang="ts" setup></script>
